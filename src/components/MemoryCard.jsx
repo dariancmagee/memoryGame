f7302f8e60eed4229e6b994c4dcf5e4a4e0110e1
dariancmagee <dariancmagee@gmail.com>
2022-03-31 21:44:20 -0400

    created memory game board

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MemoryCard.css';
+
+class MemoryCard extends React.Component {
+    render () {
+        return(
+            <div className='MemoryCard'>
+                <img src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png' alt='DC Logo'></img>
+            </div>
+        );
+    }
+}
+
+export default MemoryCard;
