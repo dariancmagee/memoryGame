5870b538cab795bb9df21b9f4f1c1ad987b1402d
dariancmagee <dariancmagee@gmail.com>
2022-04-02 12:11:21 -0400

    added card flip animation

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import './MemoryCard.css';
 
+
+
 class MemoryCard extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = { isFlipped: false };
+        
+    }
+    
+    clickHandler() {
+      this.setState({ isFlipped: !this.state.isFlipped })
+   };
+
+   
+
     render () {
+
+        const memoryCardInnerClass = 'MemoryCardInner'
+
         return(
-            <div className='MemoryCard'>
+            <div className='MemoryCard' onClick={this.clickHandler.bind(this)}>
+            <div className={!this.state.isFlipped ? memoryCardInnerClass : 'MemoryCardInner flipped'}>
+            <div className='MemoryCardBack'>
                 <img src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png' alt='DC Logo'></img>
+            </div>
+            <div className='MemoryCardFront'>∆</div>
+            </div>
             </div>
         );
     }
 }
+
 
 export default MemoryCard;
